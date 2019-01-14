89f124e3837188bd2c7c1fe242d8384f34f04b33
oshra <oshra@cavsystems.com>
2019-01-14 16:36:14 +0200

    improve styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,22 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'app';
+  previousUrl: string;
+
+  constructor(private renderer: Renderer2, private router: Router) {
+    this.router.events
+      .subscribe((event) => {
+        if (event instanceof NavigationStart) {
+          if (this.previousUrl) {
+            this.renderer.removeClass(document.body, this.previousUrl);
+          }
+          const currentUrlSlug = event.url.slice(1);
+          if (currentUrlSlug) {
+            this.renderer.addClass(document.body, currentUrlSlug);
+          }
+          this.previousUrl = currentUrlSlug;
+        }
+      });
+  }
 
 }
