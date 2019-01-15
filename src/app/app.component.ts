38dbbe1612e990a49c8cad3a4c49ef868b4e411a
oshra <oshra@cavsystems.com>
2019-01-15 11:45:29 +0200

    hebrew date in title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import { Component, Renderer2, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 
 @Component({
@@ -7,25 +7,32 @@ import { Router, NavigationStart } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
     
   title = 'app';
   previousUrl: string;
-
+  date: string;
+  
   constructor(private renderer: Renderer2, private router: Router) {
+    // adding class according to the active route. from https://arjunphp.com/angular-4-add-dynamic-class-body-based-route/
     this.router.events
-      .subscribe((event) => {
-        if (event instanceof NavigationStart) {
-          if (this.previousUrl) {
-            this.renderer.removeClass(document.body, this.previousUrl);
-          }
-          const currentUrlSlug = event.url.slice(1);
-          if (currentUrlSlug) {
-            this.renderer.addClass(document.body, currentUrlSlug);
-          }
-          this.previousUrl = currentUrlSlug;
+    .subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        if (this.previousUrl) {
+          this.renderer.removeClass(document.body, this.previousUrl);
         }
-      });
+        // remove the first slash (/)
+        const currentUrlSlug = event.url.slice(1);
+        if (currentUrlSlug) {
+          this.renderer.addClass(document.body, currentUrlSlug);
+        }
+        this.previousUrl = currentUrlSlug;
+      }
+    });
   }
-
+  
+  ngOnInit(): void {
+    this.date = new HeDate();
+  }
+  
 }
